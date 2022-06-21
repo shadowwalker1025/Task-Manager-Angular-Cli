76cf53938cbac9c34efe49d3d2930ded44ee7d94
shadowwalker1025 <nagendra.tere@gmail.com>
2022-06-21 17:21:38 +0530

    First Commit, Fully Working Task-Manager with BackEnd

---
+++ src/app/mock.tasks.ts
@@ -0,0 +1,34 @@
+import { Task } from "./Task";
+
+export const TASKS: Task[] = [
+    {
+      id: 1,
+      text: 'Doctors Appointment',
+      day: 'May 5th at 2:30pm',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Meeting at School',
+      day: 'May 6th at 1:30pm',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Food Shopping',
+      day: 'May 7th at 12:30pm',
+      reminder: false,
+    },
+    {
+      id: 4,
+      text: 'Clothes Shopping',
+      day: 'May 10th at 2:30pm',
+      reminder: false,
+    },
+    {
+      id: 5,
+      text: 'Vacation',
+      day: 'May 7th at 12:30pm',
+      reminder: false,
+    },
+  ];
